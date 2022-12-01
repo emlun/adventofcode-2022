38d0d77beb079090b7b27efac9c1d39c3bfb31f4
Emil Lundberg <emil@yubico.com>
2022-12-01 10:38:42 +0100

    Solve day 1b

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,11 +1,14 @@
+use std::collections::BinaryHeap;
+
 use crate::common::Solution;
 
 fn solve_a(elves: &[Vec<i32>]) -> i32 {
     elves.iter().map(|elf| elf.into_iter().sum()).max().unwrap()
 }
 
-fn solve_b(numbers: &[Vec<i32>]) -> usize {
-    0
+fn solve_b(elves: &[Vec<i32>]) -> i32 {
+    let mut top_elves: BinaryHeap<i32> = elves.iter().map(|elf| elf.into_iter().sum()).collect();
+    top_elves.pop().unwrap() + top_elves.pop().unwrap() + top_elves.pop().unwrap()
 }
 
 pub fn solve(lines: &[String]) -> Solution {
