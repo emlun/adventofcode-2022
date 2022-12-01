a95cfde1ff4d1778fce83f9f7135e597806d11e5
Emil Lundberg <emil@yubico.com>
2022-12-01 10:33:45 +0100

    Solve day 1a

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,5 +1,21 @@
 use crate::common::Solution;
 
+fn solve_a(elves: &[Vec<i32>]) -> i32 {
+    elves.iter().map(|elf| elf.into_iter().sum()).max().unwrap()
+}
+
+fn solve_b(numbers: &[Vec<i32>]) -> usize {
+    0
+}
+
 pub fn solve(lines: &[String]) -> Solution {
-    ("".to_string(), "".to_string())
+    let elves: Vec<Vec<i32>> = lines.iter().fold(vec![Vec::new()], |mut elves, line| {
+        match line.as_str() {
+            "" => elves.push(Vec::new()),
+            nonempty => elves.last_mut().unwrap().push(nonempty.parse().unwrap()),
+        }
+        elves
+    });
+
+    (solve_a(&elves).to_string(), solve_b(&elves).to_string())
 }
