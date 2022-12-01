77ec3226e1aba9895f842cd0067bd9d0dee7e217
Emil Lundberg <emil@yubico.com>
2022-12-01 13:16:24 +0100

    Optimize day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -2,23 +2,25 @@ use std::collections::BinaryHeap;
 
 use crate::common::Solution;
 
-fn solve_a(elves: &[Vec<i32>]) -> i32 {
-    elves.iter().map(|elf| elf.into_iter().sum()).max().unwrap()
+fn solve_a(elves: &BinaryHeap<i32>) -> &i32 {
+    elves.peek().unwrap()
 }
 
-fn solve_b(elves: &[Vec<i32>]) -> i32 {
-    let mut top_elves: BinaryHeap<i32> = elves.iter().map(|elf| elf.into_iter().sum()).collect();
-    top_elves.pop().unwrap() + top_elves.pop().unwrap() + top_elves.pop().unwrap()
+fn solve_b(mut elves: BinaryHeap<i32>) -> i32 {
+    elves.pop().unwrap() + elves.pop().unwrap() + elves.pop().unwrap()
 }
 
 pub fn solve(lines: &[String]) -> Solution {
-    let elves: Vec<Vec<i32>> = lines.iter().fold(vec![Vec::new()], |mut elves, line| {
-        match line.as_str() {
-            "" => elves.push(Vec::new()),
-            nonempty => elves.last_mut().unwrap().push(nonempty.parse().unwrap()),
-        }
-        elves
-    });
+    let elves: BinaryHeap<i32> = lines
+        .iter()
+        .fold(vec![0], |mut elves, line| {
+            match line.as_str() {
+                "" => elves.push(0),
+                nonempty => *elves.last_mut().unwrap() += nonempty.parse::<i32>().unwrap(),
+            }
+            elves
+        })
+        .into();
 
-    (solve_a(&elves).to_string(), solve_b(&elves).to_string())
+    (solve_a(&elves).to_string(), solve_b(elves).to_string())
 }
