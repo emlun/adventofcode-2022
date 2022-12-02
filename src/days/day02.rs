145c30379b935e734f7cddd906ff9eabad642484
Emil Lundberg <emil@yubico.com>
2022-12-02 15:35:06 +0100

    Further optimize day 2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,56 +1,24 @@
 use crate::common::Solution;
 
-const ROCK: i32 = 1;
-const PAPER: i32 = 2;
-const SCISSORS: i32 = 3;
-
-const LOSS: i32 = 0;
-const DRAW: i32 = 3;
-const WIN: i32 = 6;
-
-fn play_a(opp: &str, me: &str) -> i32 {
-    match (opp, me) {
-        ("A", "X") => ROCK + DRAW,
-        ("B", "Y") => PAPER + DRAW,
-        ("C", "Z") => SCISSORS + DRAW,
-
-        ("A", "Y") => PAPER + WIN,
-        ("B", "Z") => SCISSORS + WIN,
-        ("C", "X") => ROCK + WIN,
-
-        ("A", "Z") => SCISSORS + LOSS,
-        ("B", "X") => ROCK + LOSS,
-        ("C", "Y") => PAPER + LOSS,
-
-        _ => unimplemented!(),
-    }
-}
-
-fn play_b(opp: &str, result: &str) -> i32 {
-    match (opp, result) {
-        ("A", "X") => SCISSORS + LOSS,
-        ("B", "X") => ROCK + LOSS,
-        ("C", "X") => PAPER + LOSS,
-
-        ("A", "Y") => ROCK + DRAW,
-        ("B", "Y") => PAPER + DRAW,
-        ("C", "Y") => SCISSORS + DRAW,
-
-        ("A", "Z") => PAPER + WIN,
-        ("B", "Z") => SCISSORS + WIN,
-        ("C", "Z") => ROCK + WIN,
-
-        _ => unimplemented!(),
-    }
-}
+const ASCII_A: u8 = 0x41;
+const ASCII_X: u8 = 0x58;
 
 pub fn solve(lines: &[String]) -> Solution {
-    let (solution_a, solution_b) = lines.iter().fold((0, 0), |(score_a, score_b), line| {
-        let mut splits = line.split(' ');
-        let l = splits.next().unwrap();
-        let r = splits.next().unwrap();
-        (score_a + play_a(l, r), score_b + play_b(l, r))
-    });
+    let (solution_a, solution_b): (u32, u32) =
+        lines.iter().fold((0, 0), |(score_a, score_b), line| {
+            let mut splits = line.split(' ');
+            let opp = splits.next().unwrap().as_bytes()[0] - ASCII_A;
+            let r = splits.next().unwrap().as_bytes()[0] - ASCII_X;
+
+            let my_move_a = r;
+            let my_move_b = (r + opp + 2) % 3;
+            let result_a = (my_move_a + (3 - opp) + 1) % 3;
+
+            (
+                score_a + u32::from(my_move_a + 1 + result_a * 3),
+                score_b + u32::from(my_move_b + 1 + r * 3),
+            )
+        });
 
     (solution_a.to_string(), solution_b.to_string())
 }
