2b8d37db6150548b627e992c782f6fac2409817f
Emil Lundberg <emil@yubico.com>
2022-12-02 10:45:29 +0100

    Optimize day 2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,115 +1,56 @@
-use std::str::FromStr;
-
 use crate::common::Solution;
 
-#[derive(Clone, Copy)]
-enum Move {
-    Rock,
-    Paper,
-    Scissors,
-}
+const ROCK: i32 = 1;
+const PAPER: i32 = 2;
+const SCISSORS: i32 = 3;
 
-impl Move {
-    fn score(&self) -> i32 {
-        match self {
-            Self::Rock => 1,
-            Self::Paper => 2,
-            Self::Scissors => 3,
-        }
-    }
+const LOSS: i32 = 0;
+const DRAW: i32 = 3;
+const WIN: i32 = 6;
 
-    fn play(&self, other: &Move) -> PlayResult {
-        use Move::*;
-        use PlayResult::*;
-        match (self, other) {
-            (Rock, Scissors) => Win,
-            (Paper, Rock) => Win,
-            (Scissors, Paper) => Win,
-            (Rock, Paper) => Loss,
-            (Paper, Scissors) => Loss,
-            (Scissors, Rock) => Loss,
-            _ => Draw,
-        }
+fn play_a(opp: &str, me: &str) -> i32 {
+    match (opp, me) {
+        ("A", "X") => ROCK + DRAW,
+        ("B", "Y") => PAPER + DRAW,
+        ("C", "Z") => SCISSORS + DRAW,
+
+        ("A", "Y") => PAPER + WIN,
+        ("B", "Z") => SCISSORS + WIN,
+        ("C", "X") => ROCK + WIN,
+
+        ("A", "Z") => SCISSORS + LOSS,
+        ("B", "X") => ROCK + LOSS,
+        ("C", "Y") => PAPER + LOSS,
+
+        _ => unimplemented!(),
     }
 }
 
-impl FromStr for Move {
-    type Err = ();
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "A" | "X" => Ok(Self::Rock),
-            "B" | "Y" => Ok(Self::Paper),
-            "C" | "Z" => Ok(Self::Scissors),
-            _ => Err(()),
-        }
+fn play_b(opp: &str, result: &str) -> i32 {
+    match (opp, result) {
+        ("A", "X") => SCISSORS + LOSS,
+        ("B", "X") => ROCK + LOSS,
+        ("C", "X") => PAPER + LOSS,
+
+        ("A", "Y") => ROCK + DRAW,
+        ("B", "Y") => PAPER + DRAW,
+        ("C", "Y") => SCISSORS + DRAW,
+
+        ("A", "Z") => PAPER + WIN,
+        ("B", "Z") => SCISSORS + WIN,
+        ("C", "Z") => ROCK + WIN,
+
+        _ => unimplemented!(),
     }
-}
-
-enum PlayResult {
-    Win,
-    Loss,
-    Draw,
-}
-
-impl PlayResult {
-    fn score(&self) -> i32 {
-        match self {
-            Self::Win => 6,
-            Self::Loss => 0,
-            Self::Draw => 3,
-        }
-    }
-
-    fn when_opponent(&self, opp: &Move) -> Move {
-        use Move::*;
-        use PlayResult::*;
-        match (self, opp) {
-            (Win, Rock) => Paper,
-            (Win, Paper) => Scissors,
-            (Win, Scissors) => Rock,
-            (Loss, Rock) => Scissors,
-            (Loss, Paper) => Rock,
-            (Loss, Scissors) => Paper,
-            (Draw, opp) => *opp,
-        }
-    }
-}
-
-impl FromStr for PlayResult {
-    type Err = ();
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "X" => Ok(Self::Loss),
-            "Y" => Ok(Self::Draw),
-            "Z" => Ok(Self::Win),
-            _ => Err(()),
-        }
-    }
-}
-
-fn solve_a(moves: &[(Move, Move, PlayResult)]) -> i32 {
-    moves.iter().fold(0, |score, (opp, me, _)| {
-        score + me.score() + me.play(opp).score()
-    })
-}
-
-fn solve_b(moves: &[(Move, Move, PlayResult)]) -> i32 {
-    moves.iter().fold(0, |score, (opp, _, result)| {
-        let me = result.when_opponent(opp);
-        score + me.score() + result.score()
-    })
 }
 
 pub fn solve(lines: &[String]) -> Solution {
-    let moves: Vec<(Move, Move, PlayResult)> = lines
-        .iter()
-        .map(|line| {
-            let mut splits = line.split(' ');
-            let l = splits.next().unwrap();
-            let r = splits.next().unwrap();
-            (l.parse().unwrap(), r.parse().unwrap(), r.parse().unwrap())
-        })
-        .collect();
+    let (solution_a, solution_b) = lines.iter().fold((0, 0), |(score_a, score_b), line| {
+        let mut splits = line.split(' ');
+        let l = splits.next().unwrap();
+        let r = splits.next().unwrap();
+        (score_a + play_a(l, r), score_b + play_b(l, r))
+    });
 
-    (solve_a(&moves).to_string(), solve_b(&moves).to_string())
+    (solution_a.to_string(), solution_b.to_string())
 }
