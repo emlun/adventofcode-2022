8eddbe44d446a0e1204d4739ecad4dee55e08feb
Emil Lundberg <emil@emlun.se>
2022-12-03 18:51:41 +0100

    Optimize day 3

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,55 +1,51 @@
-use std::collections::HashSet;
-
 use crate::common::Solution;
 
 const ASCII_A: u8 = 0x41;
 const ASCII_A_LOW: u8 = 0x61;
 
-fn priority(c: &u8) -> i32 {
-    i32::from(if *c >= ASCII_A_LOW {
+fn char_to_flag(c: &u8) -> u64 {
+    let idx = if *c >= ASCII_A_LOW {
         c - ASCII_A_LOW + 1
     } else {
         c - ASCII_A + 27
-    })
+    };
+    1 << idx
 }
 
-fn solve_a(rucksacks: &[(HashSet<u8>, HashSet<u8>)]) -> i32 {
+fn flags_to_priority_sum(flags: u64) -> i32 {
+    (1..=52).filter(|i| flags & (1 << i) != 0).sum()
+}
+
+fn solve_a(rucksacks: &[(u64, u64)]) -> i32 {
     rucksacks
         .iter()
-        .flat_map(|(l, r)| l.intersection(r))
-        .map(priority)
+        .map(|(l, r)| l & r)
+        .map(flags_to_priority_sum)
         .sum()
 }
 
-fn solve_b(rucksacks: &[(HashSet<u8>, HashSet<u8>)]) -> i32 {
+fn solve_b(rucksacks: &[(u64, u64)]) -> i32 {
     let mut sacks = rucksacks.iter();
     let mut result = 0;
     while let (Some((la, ra)), Some((lb, rb)), Some((lc, rc))) =
         (sacks.next(), sacks.next(), sacks.next())
     {
-        result += (la.union(ra).copied().collect::<HashSet<u8>>())
-            .intersection(&lb.union(rb).copied().collect::<HashSet<u8>>())
-            .copied()
-            .collect::<HashSet<u8>>()
-            .intersection(&lc.union(rc).copied().collect::<HashSet<u8>>())
-            .map(priority)
-            .sum::<i32>();
+        result += flags_to_priority_sum((la | ra) & (lb | rb) & (lc | rc));
     }
     result
 }
 
 pub fn solve(lines: &[String]) -> Solution {
-    let rucksacks: Vec<(HashSet<u8>, HashSet<u8>)> = lines
+    let rucksacks: Vec<(u64, u64)> = lines
         .iter()
         .map(|line| {
             let (l, r) = line.split_at(line.len() / 2);
             (
-                l.as_bytes().iter().copied().collect(),
-                r.as_bytes().iter().copied().collect(),
+                l.as_bytes().iter().fold(0, |acc, c| acc | char_to_flag(c)),
+                r.as_bytes().iter().fold(0, |acc, c| acc | char_to_flag(c)),
             )
         })
         .collect();
-
     (
         solve_a(&rucksacks).to_string(),
         solve_b(&rucksacks).to_string(),
