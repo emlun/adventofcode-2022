7bcca1d512c39e83bf91c2040b2a1625338590f6
Emil Lundberg <emil@emlun.se>
2022-12-06 09:44:38 +0100

    Solve day 5b

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,4 +1,5 @@
 use crate::common::Solution;
+use std::collections::HashMap;
 
 fn solve_a(line: &str) -> usize {
     let chars: Vec<char> = line.chars().collect();
@@ -16,9 +17,29 @@ fn solve_a(line: &str) -> usize {
     unimplemented!()
 }
 
+fn solve_b(line: &str) -> usize {
+    let chars: Vec<char> = line.chars().collect();
+    let mut counts: HashMap<char, usize> = HashMap::new();
+    const N: usize = 14;
+
+    for i in 0..N {
+        *counts.entry(chars[i]).or_insert(0) += 1;
+    }
+
+    for i in N..line.len() {
+        if counts.values().filter(|v| **v > 0).count() >= N {
+            return i;
+        }
+
+        *counts.get_mut(&chars[i - N]).unwrap() -= 1;
+        *counts.entry(chars[i]).or_insert(0) += 1;
+    }
+    unimplemented!()
+}
+
 pub fn solve(lines: &[String]) -> Solution {
     let line = &lines[0];
     let solution_a = solve_a(line);
 
-    (solution_a.to_string(), "".to_string())
+    (solution_a.to_string(), solve_b(line).to_string())
 }
