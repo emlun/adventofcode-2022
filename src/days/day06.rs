6e154a9c78b1f445b53160cf7a5a508c59021a49
Emil Lundberg <emil@yubico.com>
2022-12-06 10:41:15 +0100

    Optimize day 6

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,45 +1,36 @@
 use crate::common::Solution;
-use std::collections::HashMap;
 
-fn solve_a(line: &str) -> usize {
-    let chars: Vec<char> = line.chars().collect();
-    for i in 3..line.len() {
-        if chars[i - 3] != chars[i - 2]
-            && chars[i - 3] != chars[i - 1]
-            && chars[i - 3] != chars[i]
-            && chars[i - 2] != chars[i - 1]
-            && chars[i - 2] != chars[i]
-            && chars[i - 1] != chars[i]
-        {
-            return i + 1;
+fn solve_b(line: &str, n: usize) -> usize {
+    let chars: &[u8] = line.as_bytes();
+    let mut counts: [u8; 123] = [0; 123]; // ASCII 'z' = 122
+    let mut num_nonzero = 0;
+
+    for i in 0..n {
+        if counts[usize::from(chars[i])] == 0 {
+            num_nonzero += 1;
         }
-    }
-    unimplemented!()
-}
-
-fn solve_b(line: &str) -> usize {
-    let chars: Vec<char> = line.chars().collect();
-    let mut counts: HashMap<char, usize> = HashMap::new();
-    const N: usize = 14;
-
-    for i in 0..N {
-        *counts.entry(chars[i]).or_insert(0) += 1;
+        counts[usize::from(chars[i])] += 1;
     }
 
-    for i in N..line.len() {
-        if counts.values().filter(|v| **v > 0).count() >= N {
+    for i in n..line.len() {
+        if num_nonzero >= n {
             return i;
         }
 
-        *counts.get_mut(&chars[i - N]).unwrap() -= 1;
-        *counts.entry(chars[i]).or_insert(0) += 1;
+        counts[usize::from(chars[i - n])] -= 1;
+        if counts[usize::from(chars[i - n])] == 0 {
+            num_nonzero -= 1;
+        }
+
+        if counts[usize::from(chars[i])] == 0 {
+            num_nonzero += 1;
+        }
+        counts[usize::from(chars[i])] += 1;
     }
     unimplemented!()
 }
 
 pub fn solve(lines: &[String]) -> Solution {
     let line = &lines[0];
-    let solution_a = solve_a(line);
-
-    (solution_a.to_string(), solve_b(line).to_string())
+    (solve_b(line, 4).to_string(), solve_b(line, 14).to_string())
 }
