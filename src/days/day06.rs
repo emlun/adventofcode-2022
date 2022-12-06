625a3927ea3b32f0903e12ca43ecb6db673e067d
Emil Lundberg <emil@emlun.se>
2022-12-06 09:14:00 +0100

    Solve day 6a

---
+++ src/days/day06.rs
@@ -0,0 +1,25 @@
+use crate::common::Solution;
+
+fn solve_a(line: &str) -> usize {
+    let mut i = 0;
+    let chars: Vec<char> = line.chars().collect();
+    for i in 3..line.len() {
+        if chars[i - 3] != chars[i - 2]
+            && chars[i - 3] != chars[i - 1]
+            && chars[i - 3] != chars[i]
+            && chars[i - 2] != chars[i - 1]
+            && chars[i - 2] != chars[i]
+            && chars[i - 1] != chars[i]
+        {
+            return i + 1;
+        }
+    }
+    unimplemented!()
+}
+
+pub fn solve(lines: &[String]) -> Solution {
+    let line = &lines[0];
+    let solution_a = solve_a(line);
+
+    (solution_a.to_string(), "".to_string())
+}
