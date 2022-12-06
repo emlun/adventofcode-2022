6529ff66192909ab3dbd4f3c8fc43c5a92a80fbc
Emil Lundberg <emil@yubico.com>
2022-12-06 11:11:29 +0100

    Further optimize day 6

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,36 +1,44 @@
 use crate::common::Solution;
 
-fn solve_b(line: &str, n: usize) -> usize {
-    let chars: &[u8] = line.as_bytes();
+fn solve_b(chars: &[usize], n: usize) -> usize {
     let mut counts: [u8; 123] = [0; 123]; // ASCII 'z' = 122
     let mut num_nonzero = 0;
 
     for i in 0..n {
-        if counts[usize::from(chars[i])] == 0 {
+        if counts[chars[i]] == 0 {
             num_nonzero += 1;
         }
-        counts[usize::from(chars[i])] += 1;
+        counts[chars[i]] += 1;
     }
 
-    for i in n..line.len() {
+    for i in n..chars.len() {
         if num_nonzero >= n {
             return i;
         }
 
-        counts[usize::from(chars[i - n])] -= 1;
-        if counts[usize::from(chars[i - n])] == 0 {
+        let cin = chars[i - n];
+        counts[cin] -= 1;
+        if counts[cin] == 0 {
             num_nonzero -= 1;
         }
 
-        if counts[usize::from(chars[i])] == 0 {
+        if counts[chars[i]] == 0 {
             num_nonzero += 1;
         }
-        counts[usize::from(chars[i])] += 1;
+        counts[chars[i]] += 1;
     }
     unimplemented!()
 }
 
 pub fn solve(lines: &[String]) -> Solution {
     let line = &lines[0];
-    (solve_b(line, 4).to_string(), solve_b(line, 14).to_string())
+    let chars: Vec<usize> = line
+        .as_bytes()
+        .into_iter()
+        .map(|c| usize::from(*c))
+        .collect();
+    (
+        solve_b(&chars, 4).to_string(),
+        solve_b(&chars, 14).to_string(),
+    )
 }
