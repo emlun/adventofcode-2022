a7c7e08e5ee44995d8566478564c23faa0e64f04
Emil Lundberg <emil@yubico.com>
2022-12-07 05:58:55 +0100

    Wait 1 second before downloading input

--- download-input.sh
+++ download-input.sh
@@ -9,7 +9,7 @@ else
   DAY_0=$(date --date="$YEAR-12-$1" '+%d')
 fi
 
-TRIGGER_TIMESTAMP="${YEAR}-12-${DAY_0}T05:00:00+00:00"
+TRIGGER_TIMESTAMP="${YEAR}-12-${DAY_0}T05:00:01+00:00"
 check_time() {
   [[ "$(date -Is -u)" < "${TRIGGER_TIMESTAMP}" ]]
 }
