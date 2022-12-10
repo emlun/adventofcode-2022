cc033807664b95037356499739d59ee3f16f5d83
Emil Lundberg <emil@yubico.com>
2022-12-10 12:40:52 +0100

    Clean up day 10

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,62 +1,49 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
-
 use crate::common::Solution;
-use crate::util::collections::GridCount;
+
+const TARGETS: [i32; 6] = [20, 60, 100, 140, 180, 220];
+
+fn run_cycle(
+    cycle: i32,
+    x: i32,
+    signal: i32,
+    crt_pos: i32,
+    crt: &mut Vec<char>,
+    addx: Option<i32>,
+) -> (i32, i32, i32, i32) {
+    if crt_pos == 0 {
+        crt.push('\n');
+    }
+
+    if (x - crt_pos).abs() <= 1 {
+        crt.push('#');
+    } else {
+        crt.push('.');
+    }
+
+    let new_signal = if TARGETS.contains(&cycle) {
+        signal + cycle * x
+    } else {
+        signal
+    };
+
+    (
+        cycle + 1,
+        x + addx.unwrap_or(0),
+        new_signal,
+        (crt_pos + 1) % 40,
+    )
+}
 
 pub fn solve(lines: &[String]) -> Solution {
-    let targets = vec![20, 60, 100, 140, 180, 220];
-
     let (_, _, signal_a, _, crt) = lines.iter().filter(|line| !line.is_empty()).fold(
         (1_i32, 1_i32, 0_i32, 0_i32, Vec::with_capacity(40 * 6)),
-        |(cycle, x, signal, crt_pos, mut crt), line| {
-            if crt_pos % 40 == 0 {
-                crt.push('\n');
+        |(mut cycle, mut x, mut signal, mut crt_pos, mut crt), line| {
+            (cycle, x, signal, crt_pos) = run_cycle(cycle, x, signal, crt_pos, &mut crt, None);
+            if let Some(xadd) = line.strip_prefix("addx ").map(|s| s.parse().unwrap()) {
+                (cycle, x, signal, crt_pos) =
+                    run_cycle(cycle, x, signal, crt_pos, &mut crt, Some(xadd));
             }
-
-            if (x - crt_pos).abs() <= 1 {
-                crt.push('#');
-            } else {
-                crt.push('.');
-            }
-
-            if let Some(xadd) = line.strip_prefix("addx ") {
-                if (x - ((crt_pos + 1) % 40)).abs() <= 1 {
-                    crt.push('#');
-                } else {
-                    crt.push('.');
-                }
-                if (crt_pos + 1) % 40 == 0 {
-                    crt.push('\n');
-                }
-
-                let new_signal = if targets.contains(&cycle) {
-                    dbg!(cycle, x);
-                    dbg!(signal + cycle * x)
-                } else if targets.contains(&(cycle + 1)) {
-                    dbg!(cycle, x);
-                    dbg!(signal + (cycle + 1) * x)
-                } else {
-                    signal
-                };
-
-                (
-                    cycle + 2,
-                    x + xadd.parse::<i32>().unwrap(),
-                    new_signal,
-                    (crt_pos + 2) % 40,
-                    crt,
-                )
-            } else {
-                let new_signal = if targets.contains(&cycle) {
-                    dbg!(cycle, x);
-                    dbg!(signal + cycle * x)
-                } else {
-                    signal
-                };
-
-                (cycle + 1, x, new_signal, (crt_pos + 1) % 40, crt)
-            }
+            (cycle, x, signal, crt_pos, crt)
         },
     );
     let sol_b: String = crt.into_iter().collect();
