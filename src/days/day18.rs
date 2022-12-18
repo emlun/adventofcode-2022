9ae8de27008c05e502619ae36a31d4a5a1e1cfeb
Emil Lundberg <emil@yubico.com>
2022-12-18 18:16:38 +0100

    Optimize day 18

--- src/days/day18.rs
+++ src/days/day18.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 use crate::common::Solution;
 
-type Point = (i32, i32, i32);
+type Point = (i16, i16, i16);
 
 const DXYZ: [Point; 6] = [
     (-1, 0, 0),
@@ -13,28 +13,40 @@ const DXYZ: [Point; 6] = [
     (0, 0, 1),
 ];
 
-fn solve_a(points: &HashSet<Point>) -> usize {
+fn solve_a(points: &HashSet<u64>, minx: i16, miny: i16, minz: i16) -> usize {
     points
         .iter()
+        .map(|h| unkey(*h, minx, miny, minz))
         .map(|(x, y, z)| {
             6 - DXYZ
                 .iter()
                 .map(|(dx, dy, dz)| (x + dx, y + dy, z + dz))
-                .filter(|p| points.contains(p))
+                .filter(|p| points.contains(&tokey(*p, minx, miny, minz)))
                 .count()
         })
         .sum()
 }
 
-fn solve_b(points: &HashSet<Point>) -> usize {
-    let minx = points.iter().map(|(x, _, _)| x).min().unwrap() - 1;
-    let miny = points.iter().map(|(_, y, _)| y).min().unwrap() - 1;
-    let minz = points.iter().map(|(_, _, z)| z).min().unwrap() - 1;
+fn tokey((x, y, z): Point, minx: i16, miny: i16, minz: i16) -> u64 {
+    (((x - minx) as u64) << 32) | (((y - miny) as u64) << 16) | ((z - minz) as u64)
+}
 
-    let maxx = points.iter().map(|(x, _, _)| x).max().unwrap() + 1;
-    let maxy = points.iter().map(|(_, y, _)| y).max().unwrap() + 1;
-    let maxz = points.iter().map(|(_, _, z)| z).max().unwrap() + 1;
+fn unkey(h: u64, minx: i16, miny: i16, minz: i16) -> Point {
+    let x = ((h >> 32) & 0xffff) as i16 + minx;
+    let y = ((h >> 16) & 0xffff) as i16 + miny;
+    let z = (h & 0xffff) as i16 + minz;
+    (x, y, z)
+}
 
+fn solve_b(
+    points: &HashSet<u64>,
+    minx: i16,
+    maxx: i16,
+    miny: i16,
+    maxy: i16,
+    minz: i16,
+    maxz: i16,
+) -> usize {
     let lx = maxx - minx + 1;
     let ly = maxy - miny + 1;
     let lz = maxz - minz + 1;
@@ -42,35 +54,31 @@ fn solve_b(points: &HashSet<Point>) -> usize {
 
     let cap = usize::try_from(lx * ly * lz).unwrap();
     let mut frontier: Vec<Point> = Vec::with_capacity(cap);
-    let mut outer_points: HashSet<Point> = HashSet::with_capacity(cap);
+    let mut outer_points: HashSet<u64> = HashSet::with_capacity(cap);
 
     frontier.push((minx, miny, minz));
     while let Some((x, y, z)) = frontier.pop() {
         for (dx, dy, dz) in DXYZ {
             let (nx, ny, nz) = (x + dx, y + dy, z + dz);
             let next = (nx, ny, nz);
-            if nx >= minx
-                && nx <= maxx
-                && ny >= miny
-                && ny <= maxy
-                && nz >= minz
-                && nz <= maxz
-                && !points.contains(&next)
-                && !outer_points.contains(&next)
-            {
-                frontier.push(next);
-                outer_points.insert(next);
+            if nx >= minx && nx <= maxx && ny >= miny && ny <= maxy && nz >= minz && nz <= maxz {
+                let h = tokey(next, minx, miny, minz);
+                if !points.contains(&h) && !outer_points.contains(&h) {
+                    frontier.push(next);
+                    outer_points.insert(h);
+                }
             }
         }
     }
 
     outer_points
         .iter()
-        .map(|(x, y, z)| {
+        .map(|h| {
+            let (x, y, z) = unkey(*h, minx, miny, minz);
             6 - DXYZ
                 .iter()
                 .map(|(dx, dy, dz)| (x + dx, y + dy, z + dz))
-                .filter(|p| outer_points.contains(p))
+                .filter(|p| outer_points.contains(&tokey(*p, minx, miny, minz)))
                 .count()
         })
         .sum::<usize>()
@@ -78,7 +86,7 @@ fn solve_b(points: &HashSet<Point>) -> usize {
 }
 
 pub fn solve(lines: &[String]) -> Solution {
-    let droplet: HashSet<Point> = lines
+    let points: Vec<Point> = lines
         .iter()
         .filter(|line| !line.is_empty())
         .map(|line| {
@@ -91,5 +99,21 @@ pub fn solve(lines: &[String]) -> Solution {
         })
         .collect();
 
-    (solve_a(&droplet).to_string(), solve_b(&droplet).to_string())
+    let minx = points.iter().map(|(x, _, _)| x).min().unwrap() - 1;
+    let miny = points.iter().map(|(_, y, _)| y).min().unwrap() - 1;
+    let minz = points.iter().map(|(_, _, z)| z).min().unwrap() - 1;
+
+    let maxx = points.iter().map(|(x, _, _)| x).max().unwrap() + 1;
+    let maxy = points.iter().map(|(_, y, _)| y).max().unwrap() + 1;
+    let maxz = points.iter().map(|(_, _, z)| z).max().unwrap() + 1;
+
+    let droplet: HashSet<u64> = points
+        .into_iter()
+        .map(|p| tokey(p, minx, miny, minz))
+        .collect();
+
+    (
+        solve_a(&droplet, minx, miny, minz).to_string(),
+        solve_b(&droplet, minx, maxx, miny, maxy, minz, maxz).to_string(),
+    )
 }
