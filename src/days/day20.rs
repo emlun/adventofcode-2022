3d1665eb8e1171c722a1166b7ea1d14dd346d8c8
Emil Lundberg <emil@emlun.se>
2022-12-20 14:33:40 +0100

    Clean up day 20

--- src/days/day20.rs
+++ src/days/day20.rs
@@ -2,6 +2,7 @@ use crate::common::Solution;
 
 fn mix(nums: &[isize], times: usize) -> Vec<isize> {
     let mut file: Vec<(usize, isize)> = nums.iter().copied().enumerate().collect();
+    let n = isize::try_from(file.len() - 1).unwrap();
     for _ in 0..times {
         for orig_i in 0..nums.len() {
             let i = file
@@ -11,15 +12,12 @@ fn mix(nums: &[isize], times: usize) -> Vec<isize> {
                 .map(|(i, _)| i)
                 .unwrap();
             let (orig_i, v) = file.remove(i);
-            let new_i: isize = isize::try_from(i).unwrap() + v;
-            if new_i == isize::try_from(file.len()).unwrap() {
+            let new_i: usize =
+                usize::try_from((isize::try_from(i).unwrap() + v).rem_euclid(n)).unwrap();
+            if new_i == 0 {
                 file.push((orig_i, v));
             } else {
-                file.insert(
-                    usize::try_from(new_i.rem_euclid(isize::try_from(file.len()).unwrap()))
-                        .unwrap(),
-                    (orig_i, v),
-                )
+                file.insert(new_i, (orig_i, v))
             }
         }
     }
@@ -27,42 +25,17 @@ fn mix(nums: &[isize], times: usize) -> Vec<isize> {
     file.into_iter().map(|(_, v)| v).collect()
 }
 
-fn solve_a(nums: &[isize]) -> isize {
-    let mixed = mix(nums, 1);
-
-    let mut it = mixed
+fn solve_b(nums: &[isize], key: isize, times: usize) -> isize {
+    mix(&nums.iter().map(|i| i * key).collect::<Vec<isize>>(), times)
         .into_iter()
         .cycle()
         .skip_while(|i| *i != 0)
         .step_by(1000)
-        .skip(1);
-    let first = it.next().unwrap();
-    let second = it.next().unwrap();
-    let third = it.next().unwrap();
-    first + second + third
+        .skip(1)
+        .take(3)
+        .sum()
 }
 
-fn solve_b(nums: &[isize]) -> isize {
-    const DECRYPTION_KEY: isize = 811589153;
-    let mixed = mix(
-        &nums
-            .iter()
-            .map(|i| i * DECRYPTION_KEY)
-            .collect::<Vec<isize>>(),
-        10,
-    );
-
-    let mut it = mixed
-        .into_iter()
-        .cycle()
-        .skip_while(|i| *i != 0)
-        .step_by(1000)
-        .skip(1);
-    let first = it.next().unwrap();
-    let second = it.next().unwrap();
-    let third = it.next().unwrap();
-    first + second + third
-}
 pub fn solve(lines: &[String]) -> Solution {
     let nums: Vec<isize> = lines
         .iter()
@@ -70,5 +43,8 @@ pub fn solve(lines: &[String]) -> Solution {
         .map(|s| s.parse().unwrap())
         .collect();
 
-    (solve_a(&nums).to_string(), solve_b(&nums).to_string())
+    (
+        solve_b(&nums, 1, 1).to_string(),
+        solve_b(&nums, 811589153, 10).to_string(),
+    )
 }
