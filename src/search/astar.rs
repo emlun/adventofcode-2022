841b6fc680c0d7a4917fcc12df2b5e328df36c35
Emil Lundberg <emil@yubico.com>
2023-01-04 01:29:04 +0100

    Extract function astar_optimize from day 16

--- src/search/astar.rs
+++ src/search/astar.rs
@@ -83,3 +83,44 @@ where
 
     None
 }
+
+pub fn astar_optimize<S>(initial_state: S) -> S::Value
+where
+    S: State,
+    S::Value: Clone,
+{
+    let mut queue: BinaryHeap<StateOrd<S>> = BinaryHeap::new();
+    let mut visited: HashMap<S::DuplicationKey, S::Value> = HashMap::new();
+    let mut best = initial_state.value();
+
+    queue.push(StateOrd(initial_state));
+
+    while let Some(StateOrd(state)) = queue.pop() {
+        if state.estimate() > best {
+            return best;
+        } else if visited
+            .get(&state.duplication_key())
+            .map(|v| state.value() <= *v)
+            .unwrap_or(true)
+        {
+            for next_state in state.generate_moves() {
+                let dk = next_state.duplication_key();
+                let nv = next_state.value();
+                best = std::cmp::min(best, nv.clone());
+                match visited.entry(dk) {
+                    Entry::Occupied(mut occ) if nv < *occ.get() => {
+                        occ.insert(nv);
+                        queue.push(StateOrd(next_state));
+                    }
+                    Entry::Vacant(vac) => {
+                        vac.insert(nv);
+                        queue.push(StateOrd(next_state));
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    best
+}
